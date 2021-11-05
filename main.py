6ea3c70f4acda7c7053ce0fc40e2e58a7991700e
Gal <koazgal@gmail.com>
2021-11-05 14:45:34 +0200

    update main

--- main.py
+++ main.py
@@ -2,6 +2,26 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json as js
+
+with open('D:\\B5.json', 'r') as f:
+    data = f.read()
+
+# parse file
+obj = js.loads(data)
+print("MIN FLOOR", str(obj['_minFloor']))
+print("MAX FLOOR", str(obj['_maxFloor']))
+list = obj['_elevators']
+list_size = len(list)
+for i in range(list_size):
+    print("ID:", list[i].get("_id"))
+    print("SPEED:", list[i].get("_speed"))
+    print("MIN FLOOR", list[i].get("_minFloor"))
+    print("MAX FLOOR", list[i].get("_maxFloor"))
+    print("CLOSE TIME", list[i].get("_closeTime"))
+    print("OPEN TIME", list[i].get("_openTime"))
+    print("START TIME", list[i].get("_startTime"))
+    print("STOP TIME", list[i].get("_stopTime"))
 
 
 def print_hi(name):
