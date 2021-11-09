673a2aaa8c72cb27890651d82c7f43bff8ce91eb
ShauliTaragin <shaulitaragin@gmail.com>
2021-11-09 23:09:21 +0200

    added classes for call for elevator and elevator

---
+++ CallForElevator.py
@@ -0,0 +1,10 @@
+class CallForElevator:
+    def __init__(self, time,src ,des , elev):
+        self.time = time
+        self.src = src
+        self.des = des
+        self.elev = elev
+    
+   
+    def setelev(self , elev):
+        self.elev = elev
---
+++ Elevator.py
@@ -0,0 +1,10 @@
+class Elevator:
+    def __init__(self, _id,_speed,_minFloor,_maxFloor,_closeTime,_openTime,_startTime,_stopTime):
+        self._id = _id
+        self._speed = _speed
+        self._minFloor=_minFloor
+        self._maxFloor=_maxFloor
+        self._closeTime=_closeTime
+        self._openTime=_openTime
+        self._startTime=_startTime
+        self._stopTime=_stopTime
